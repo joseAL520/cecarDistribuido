ba76064750eb9c68bc85add2efe4cc4410e94bef
jose <jose.albelt1817@gmail.com>
2022-11-20 22:49:07 -0500

    registro echo

--- src/app/page/regstuden/regstuden.component.ts
+++ src/app/page/regstuden/regstuden.component.ts
@@ -11,35 +11,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./regstuden.component.css']
 })
 
-                              // implements OnInit
+
 export class RegstudenComponent  {
-  checkFamale:boolean=false;
-  checkMas:boolean=true;
+  submitted = false;
   formulario!: FormGroup;
-  submitted=false;
   studen : any=[];
-  //createUsuario: FormGroup;
-
-
- 
- /* constructor(private fb: FormBuilder) {
- 
-    this.createUsuario = this.fb.group({
-      Nombres: ['',Validators.required],
-      Apellidos: ['',Validators.required],
-      Tdocumento: ['',Validators.required],
-      Direccion: ['',Validators.required],
-      EstadoCivil: ['',Validators.required],
-      Nacimiento: ['',Validators.required]
-      
-    })
-  }*/
- 
 
   constructor (private _http:HttpClient, private formBuilder:FormBuilder){}
 
   ngOnInit(): void {
-    
+     
     this.formulario = this.formBuilder.group({ 
       
       Nombres: ['',[Validators.required]],
@@ -47,7 +28,11 @@ export class RegstudenComponent  {
       Tdocumento: ['',[Validators.required]],
       Nacimiento: ['',[Validators.required]],
       Direccion: ['',[Validators.required]],
-      EstadoCivil: ['',[Validators.required]]
+      EstadoCivil: ['',[Validators.required]],
+      Identificacion: ['',[Validators.required]],
+      Celular: ['',[Validators.required]],
+      Correo: ['',[Validators.required]],
+      Genero: ['',[Validators.required]]
 
     });
     
@@ -68,18 +53,25 @@ export class RegstudenComponent  {
   */
   
 
-  add(values: { Nombres: any; Apellidos: any; Tdocumento: any; Nacimiento: any; Direccion: any; EstadoCivil: any; }){
+  add(values: { Nombres: any; Celular: any; Correo: any; Apellidos: any; Tdocumento: any; Nacimiento: any; Direccion: any; Genero: any; EstadoCivil: any; Identificacion: any; }){
     this.submitted=true;
     if(this.formulario.invalid){
-     return;
-    } 
+      alert("debe que llenar todo los campos");
+      return;
+    }
     var json={
-     "eNombre":values.Nombres,
-     "eApellido":values.Apellidos,
-     "eTipodocumento":values.Tdocumento,
-     "eFechaNacimiento": values.Nacimiento,
-     "eDireccion": values.Direccion,
-     "eEstadoCivil": values.EstadoCivil
+      data:{
+        "eNombre":values.Nombres,
+        "eCelular":values.Celular,
+        "eCorreo":values.Correo,
+        "eApellido":values.Apellidos,
+        "eTipodocumento":values.Tdocumento,
+        "eFechaNacimiento": values.Nacimiento,
+        "eDireccion": values.Direccion,
+        "eGenero":values.Genero,
+        "eEstadoCivil": values.EstadoCivil,
+        "eIdentificacion":values.Identificacion
+      }
     }
 
     var link = "http://172.177.150.81:1337/api/students";
@@ -87,29 +79,6 @@ export class RegstudenComponent  {
       alert("Se agrego El estudiate Existosamente");
       location.reload();
     })
-   }
+  }
 
-
- /*
- agregarUsuario(){
-    const usuario: any = {
-      Nombres: this.createUsuario.value.Nombres,
-      Apellidos: this.createUsuario.value.Apellidos,
-      Tdocumento: this.createUsuario.value.Tdocumento,
-      Nacimiento: this.createUsuario.value.Nacimiento,
-      Direccion: this.createUsuario.value.Direccion,
-      EstadoCivil: this.createUsuario.value.EstadoCivil,
-      fecheCreacionStuden: new Date()
-    }
-    console.log(usuario)
-  }
- */
-  onCheckedMas(){
-    this.checkMas = true;
-    this.checkFamale = false;
-  }
-  onCheckedFamale(){
-    this.checkMas = false;
-    this.checkFamale = true;
-  }
 }
