9a6530b77dc73008edeedbfd056c42ceba0d76d8
jose <jose.albelt1817@gmail.com>
2022-11-22 16:27:38 -0500

    color a la tabla

--- src/app/model/estudiante.ts
+++ src/app/model/estudiante.ts
@@ -1,21 +1,6 @@
 export class Estudiante{
     
-    /*static estudianteDesdeJson(obj: Object){
-        return new Estudiante(
-            obj['eNombre'],
-            obj['eCelular'],
-            obj['eCorreo'],
-            obj['eApellido'],
-            obj['eFechaNacimiento'],
-            obj['eDireccion'],
-            obj['eTipodocumento'],
-            obj['eGenero'],
-            obj['eEstadoCivil'],
-            obj['eIdentificacion'],
-        );
-    }*/
-    
-
+   
     constructor(
        public eNombre:          string,
        public eCelular:         string,
